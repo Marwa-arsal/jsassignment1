2f150fef743122ea0d36f5fff0e9002d3197ff94
Marwa-arsal <marwa.arsal@hotmail.com>
2024-12-20 12:41:34 -0800

    jswork

---
+++ apps.js
@@ -0,0 +1,14 @@
+// chapter # 01
+alert ("welcome here");
+// q#2
+alert("Error! Please enter a valid password.");
+// q#3
+alert("Welcome to JS land....\n Happing coding!");
+// q#4
+alert("Welcome to JS land...");
+alert("Happing coding! \n Prevent this page from creating additional dilogs.");
+alert("Hello...I can run JS through my web browser'console")
+var answer = prompt("What does HTML stand for?")
+if (x==="hyper text markup language") {
+    alert("correct") ;
+}
